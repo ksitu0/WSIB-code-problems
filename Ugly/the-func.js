f086f5ef9c0030230d0c4d4a35798325cbbc65a6
ksitu0 <karenhj.situ@gmail.com>
2022-02-01 13:56:07 -0800

    no more counters

--- Ugly/the-func.js
+++ Ugly/the-func.js
@@ -1,40 +1,49 @@
-function NotAConstructor(thisisastr1nglol){
-    document.body.innerHTML+="<div id='thisisastr1nglol'></div>";
-    document.getElementById("thisisastr1nglol").innerHTML = thisisastr1nglol;
+// some js and dom stuff
+function Component(hehehehehehe){
+    document.body.innerHTML+=`<div id="hehehehehehe"></div>`;
+    document.getElementById("hehehehehehe").innerHTML = hehehehehehe;
 
     document.body.innerHTML+="<div class='hehehehe'></div>";
-    for(var i=0; i<document.getElementById("thisisastr1nglol").innerHTML.length; i++){
+    for(document.querySelector("div:first-child > *:first-child").style.top="0"; parseInt(document.querySelector("div:first-child > *:first-child").style.top, 10)<document.getElementById("hehehehehehe").innerHTML.length; 
+            document.querySelector("div:first-child > *:first-child").style.top = (parseInt(document.querySelector("div:first-child > *:first-child").style.top.substring(0, document.querySelector("div:first-child > *:first-child").style.top.length-2), 10)+1).toString(10)+"px"){
         document.querySelector(".hehehehe").innerHTML += "<div class='hehehehehe'></div>"
-        for ( var j = 0; j < Math.floor(Math.random()*1723); j++ ) {
+        for (document.querySelector("div:first-child > *:first-child").style.left="0"; parseInt(document.querySelector("div:first-child > *:first-child").style.left, 10)< Math.floor(Math.random()*1723); 
+        document.querySelector("div:first-child > *:first-child").style.left = (parseInt(document.querySelector("div:first-child > *:first-child").style.left.substring(0, document.querySelector("div:first-child > *:first-child").style.left.length-2), 10)+1).toString(10)+"px") {
             document.querySelector(".hehehehehe:last-child").innerHTML += 'lawkeirhaiwprhliiueyrp93q48rh234qon8rycno8rieuwrow8y'.charAt(Math.floor(Math.random() * 'lawkeirhaiwprhliiueyrp93q48rh234qon8rycno8rieuwrow8y'.length));
         }
     }
-    document.body.innerHTML+="<div class='idksomething'></div>";
-    for(var i=0; i<document.getElementById("thisisastr1nglol").innerHTML.length; i++){
+    document.body.innerHTML+="<div class='hehehehehehehe'></div>";
+    for(document.querySelector("div:first-child > *:first-child").style.right="0"; parseInt(document.querySelector("div:first-child > *:first-child").style.right, 10)<document.getElementById("hehehehehehe").innerHTML.length; 
+    document.querySelector("div:first-child > *:first-child").style.right = (parseInt(document.querySelector("div:first-child > *:first-child").style.right.substring(0, document.querySelector("div:first-child > *:first-child").style.right.length-2), 10)+1).toString(10)+"px"){
         try {
-            document.querySelector(".idksomething").innerHTML += "<div class='" + document.querySelectorAll(".hehehehehe")[i].innerHTML+`${i}` + "'>" + document.getElementById("thisisastr1nglol").innerHTML.substring(i, i+1) + "</div>";
+            document.querySelector(".hehehehehehehe").innerHTML += "<div class='" + document.querySelectorAll(".hehehehehe")[parseInt(document.querySelector("div:first-child > *:first-child").style.right.substring(0, document.querySelector("div:first-child > *:first-child").style.right.length-2), 10)].innerHTML+`${parseInt(document.querySelector("div:first-child > *:first-child").style.right.substring(0, document.querySelector("div:first-child > *:first-child").style.right.length-2), 10)}` + "'>" + document.getElementById("hehehehehehe").innerHTML.substring(parseInt(document.querySelector("div:first-child > *:first-child").style.right.substring(0, document.querySelector("div:first-child > *:first-child").style.right.length-2), 10), parseInt(document.querySelector("div:first-child > *:first-child").style.right.substring(0, document.querySelector("div:first-child > *:first-child").style.right.length-2), 10)+1) + "</div>";
         } catch(e) {
-            document.querySelector(".idksomething").appendChild(document.createElement('div'));
-            document.querySelector(".idksomething > div:last-child").classList.add(document.querySelectorAll(".hehehehehe")[i].innerHTML+`${i}`);
-            document.querySelector(".idksomething > div:last-child").innerText = document.getElementById("thisisastr1nglol").innerHTML.substring(i)
+            document.querySelector(".hehehehehehehe").appendChild(document.createElement('div'));
+            document.querySelector(".hehehehehehehe > div:last-child").classList.add(document.querySelectorAll(".hehehehehe")[parseInt(document.querySelector("div:first-child > *:first-child").style.right.substring(0, document.querySelector("div:first-child > *:first-child").style.right.length-2), 10)].innerHTML+`${parseInt(document.querySelector("div:first-child > *:first-child").style.right.substring(0, document.querySelector("div:first-child > *:first-child").style.right.length-2), 10)}`);
+            document.querySelector(".hehehehehehehe > div:last-child").innerText = document.getElementById("hehehehehehe").innerHTML.substring(parseInt(document.querySelector("div:first-child > *:first-child").style.right.substring(0, document.querySelector("div:first-child > *:first-child").style.right.length-2), 10))
         }
     }
-    document.body.innerHTML+="<div class='thisisastrlnglol'></div>";
+    document.body.innerHTML+="<div class='hehehehehehehehe'></div>";
     console.log(parseInt(((((document.querySelectorAll(".hehehehehe").length-120391823 + 120391822)*5-50)*20+1014)/100).toString().substring(0, ((((document.querySelectorAll(".hehehehe > .hehehehehe").length-120391823 + 120391822)*5-50)*20+1014)/100).toString().indexOf("."))))
-    for(var i=parseInt(((((document.querySelectorAll(".hehehehehe").length-120391823 + 120391822)*5-50)*20+1014)/100).toString().substring(0, ((((document.querySelectorAll(".hehehehe > .hehehehehe").length-120391823 + 120391822)*5-50)*20+1014)/100).toString().indexOf("."))); i>=0; i--){
-        document.querySelector(".thisisastrlnglol").innerHTML += JSON.parse(`{"`+[...document.querySelectorAll(".idksomething > *")].map((qwert) => `${qwert.classList[0]}": "${qwert.innerHTML}`).join("\",\"") +"\"}")[document.querySelectorAll(".hehehehehe")[i].innerHTML+`${i}`];
+    for(document.querySelector("div:first-child > *:first-child").style.bottom = 
+            parseInt(((((document.querySelectorAll(".hehehehehe").length-120391823 + 120391822)*5-50)*20+1014)/100).toString().substring(0, ((((document.querySelectorAll(".hehehehe > .hehehehehe").length-120391823 + 120391822)*5-50)*20+1014)/100).toString().indexOf("."))).toString()+"px";
+    parseInt(document.querySelector("div:first-child > *:first-child").style.bottom)>=0; document.querySelector("div:first-child > *:first-child").style.bottom = (parseInt(document.querySelector("div:first-child > *:first-child").style.bottom)-1).toString()+"px")
+    {document.querySelector(".hehehehehehehehe").innerHTML += JSON.parse(`{"`+[...document.querySelectorAll(".hehehehehehehe > *")].map((qwert) => `${qwert.classList[0]}": "${qwert.innerHTML}`).join("\",\"") +"\"}")[document.querySelectorAll(".hehehehehe")[parseInt(document.querySelector("div:first-child > *:first-child").style.bottom)].innerHTML+`${parseInt(document.querySelector("div:first-child > *:first-child").style.bottom)}`];
     }
-    document.querySelector(".thisisastrlnglol").innerHTML= document.querySelector(".thisisastrlnglol").innerHTML.split('').reduce(function(haveyouever, eatenanegg){return haveyouever.concat('', eatenanegg)});
-    document.body.innerHTML+="<canvas id='idksomething'><canvas class='yo'></canvas></canvas>";
-    for(var i=0; i<document.querySelector(".thisisastrlnglol").innerHTML.length; i++){
-        if(document.querySelectorAll(".thisisastrlnglol")[0].innerHTML.substring(i) === document.getElementById("thisisastr1nglol").innerHTML.substring(i)){
+    document.querySelector(".hehehehehehehehe").innerHTML= document.querySelector(".hehehehehehehehe").innerHTML.split('').reduce(function(haveyouever, eatenanegg){return haveyouever.concat('', eatenanegg)});
+    document.body.innerHTML+="<canvas id='hehehehehehehe'><canvas class='Class'></canvas></canvas>";
+    for(var i=0; i<document.querySelector(".hehehehehehehehe").innerHTML.length; i++){
+        if(document.querySelectorAll(".hehehehehehehehe")[0].innerHTML.substring(i) === document.getElementById("hehehehehehe").innerHTML.substring(i)){
             console.log();
         } else {
             console.log("wrong!")
-            document.querySelector("#idksomething").innerHTML = document.querySelector("#idksomething").innerHTML.replace(/canvas/g, "iframe");
+            document.querySelector("#hehehehehehehe").innerHTML = document.querySelector("#hehehehehehehe").innerHTML.replace(/canvas/g, "iframe");
             break;
         }
     }
-    document.querySelectorAll("div:last-child")[0].innerHTML+= "<button>" + (document.querySelector("#idksomething > *").tagName.toLowerCase() === "canvas")+"</button>";
-    return !(document.querySelector("#idksomething > *").tagName.toLowerCase() === "iframe");
+    document.querySelectorAll("div:last-child")[0].innerHTML+= "<button>" + (document.querySelector("#hehehehehehehe > *").tagName.toLowerCase() === "canvas")+"</button>";
+    document.getElementsByTagName("title")[0].innerHTML= document.querySelector("#hehehehehehehe > *").tagName.toLowerCase();
+    document.body.innerHTML = document.body.innerHTML.substring(0, document.body.innerHTML.substring(0, document.body.innerHTML.indexOf(`id="hehehehehehe"`)).lastIndexOf("<"));
+    console.log(!(document.getElementsByTagName("title")[0].innerHTML === "iframe"), hehehehehehe);
+    return !(document.getElementsByTagName("title")[0].innerHTML === "iframe");
 }
